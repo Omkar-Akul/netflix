451c55733274cc9cc3bdde14c5855b991c2a578f
Omkar-Akul <omkarakul1907@gmail.com>
2026-02-24 11:25:22 +0530

    up

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Email from "next-auth/providers/email";
+import { error } from "node:console";
+import prismadb from "../../lib/prismadb";
+import {compare} from 'bcrypt';
+
 
 export default NextAuth ({
   providers: [
@@ -12,15 +16,42 @@ export default NextAuth ({
          label: 'Email',
          type: 'text',
 
-        };
+        },
         password:{
           label: 'Password',
           type: 'password'
         }
       },
-      async authorize () {
-       
+      async authorize(credentials) {
+       if (!credentials?.email || !credentials?.password){
+        throw new Error('email and password are required');
+       }
+       const user = await prismadb.user.findUnique({
+        where: {
+          email: credentials.email
+        }
+       });
+
+       if(!user || !user.hashedPassword){
+        throw new Error('email does not exist');
+       } 
+       const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+       if(!isCorrectPassword){
+        throw new Error('incorrect password');
+       }
+       return user
       }
     })
-  ]
+  ],
+  pages:{
+    signIn:'/auth',
+  },
+  debug:process.env.NODE_ENV === 'development',
+  session:{
+    strategy:'jwt',
+  },
+  jwt: {
+    secret:'process.env.NEXTAUTH_JWT_SECRET'
+  },
+  secret: 'process.env.NEXTAUTH_SECRET'
 })
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useState } from "react";
 import Input from "../components/input";
+import axios from 'axios';
 
 const Auth = () => {
   const [email,setEmail] = useState('');
@@ -12,6 +13,18 @@ const Auth = () => {
       setVariant((currentVariant) => currentVariant == 'login' ? 'register' : 'login',)
     }, [])
 
+    const register = useCallback(async () => {
+      try{
+        await axios.post('/api/register',{
+          email,
+          name,
+          password,
+        });
+      } catch(error){
+        console.log(error);
+      }
+      
+    }, [email,name,password]);
 
   return (
     <div className="relative h-screen w-full bg-cover bg-center bg-fixed bg-no-repeat" style={{backgroundImage: "url(/images/hero.jpg)"}}>
@@ -22,10 +35,10 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:mx-w-md lg:w-2/5 rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant == 'login' ? 'sign in': 'create an account'}
+              {variant === 'login' ? 'sign in': 'create an account'}
               </h2>
               <div className="flex flex-col gap-4">
-                {variant == 'register' && ( 
+                {variant === 'register' && ( 
                   <Input 
                     label="Username"
                     onChange={(ev: any) => setName(ev.target.value)}
@@ -49,12 +62,12 @@ const Auth = () => {
                   type="password"
                 />
               </div>
-              <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"> 
+              <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"> 
               {variant == 'login' ? 'Login' : ' Sign up'}
               </button>
               <p className="text-neutral-500 mt-12">
-                {variant == 'login' ? 'First time using netflix?' : 'Already have an account?'}<span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                  {variant == 'login' ? 'Create an account' : 'Login'}
+                {variant === 'login' ? 'First time using netflix?' : 'Already have an account?'}<span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                  {variant === 'login' ? 'Create an account' : 'Login'}
                 </span>
               </p>
           </div>
